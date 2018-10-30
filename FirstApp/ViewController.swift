1fda9c295ca20c0831fed7c9a6ca4f6ff14876f8
Yuda Eka Putra <yuda702@gmail.com>
2018-10-30 16:33:23 +0700

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Yuda Eka Putra on 30/10/18.
+//  Copyright © 2018 FreakyCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
